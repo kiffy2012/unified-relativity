b0610c7e7814e4be14d9e44ebc775d647f8a277e
kiffy2012 <131360254+kiffy2012@users.noreply.github.com>
2025-08-01 19:47:50 +0100

    Add mass-based forces and object settings

--- space_object.py
+++ space_object.py
@@ -4,7 +4,8 @@ import numpy as np
 from PyQt5.QtGui import QVector3D
 
 class SpaceObject:
-    def __init__(self, position, radius, color):
+    def __init__(self, position, radius, color, mass=1.0):
         self.position = position
         self.radius = radius
         self.color = color
+        self.mass = mass
