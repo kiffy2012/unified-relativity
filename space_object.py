3f3d24f9ee7fe33383e3f04a556782398c2caa17
kiffy2012 <131360254+kiffy2012@users.noreply.github.com>
2025-08-01 22:00:02 +0100

    Adjust force displacement and restore original zoom

--- space_object.py
+++ space_object.py
@@ -1,10 +1,10 @@
 # space_object.py
-# space_object.py
 import numpy as np
 from PyQt5.QtGui import QVector3D
 
 class SpaceObject:
-    def __init__(self, position, radius, color):
+    def __init__(self, position, radius, color, mass=1.0):
         self.position = position
         self.radius = radius
         self.color = color
+        self.mass = mass
